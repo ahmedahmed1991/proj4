bef64502a6f37ee30d1e43ef0c9107c7d6b48c5b
ahmed <ahmed.ahmed1991@gmail.com>
2014-12-18 17:41:06 -0500

    edit

--- app/config/local/database.php
+++ app/config/local/database.php
@@ -25,7 +25,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'cars',
 			'username'  => 'root',
-			'password'  => 'root',
+			'password'  => 'kMxPkKnl3l',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
